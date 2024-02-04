a95b63d5c8fe2a8bb03cbc5beada95f28fff7d84
kebilly <billyke@BillydeMacBook-Pro.local>
2024-02-05 01:34:18 +0800

    Initial commit

---
+++ src/main/java/org/billy/Hello.java
@@ -0,0 +1,2 @@
+package org.billy;public class Hello {
+}
---
+++ src/main/java/org/billy/Hello.kt
@@ -0,0 +1,13 @@
+package org.billy
+
+fun main(){
+//    println("Hello");
+//    Human().hello()
+    val h = Human()
+    h.hello()
+}
+class Human{
+    fun hello(){
+        println("Hello Kotlin")
+    }
+}
